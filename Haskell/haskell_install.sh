5ca39b4824adb47898215874ba02e57fed45a35b
mlite <mlite@users.noreply.github.com>
2015-01-01 15:56:25 -0800

    download ghc

--- Haskell/haskell_install.sh
+++ Haskell/haskell_install.sh
@@ -1,5 +1,15 @@
 #!/bin/bash 
 
+# download ghc
+wget https://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-src.tar.bz2
+
+bzip2 -dc ghc-7.8.3-src.tar.bz2 | tar xvf -
+
+cd ghc-7.8.3
+mkdir /home/$USER/local
+./configure --prefix=/home/$USER/local
+make install
+
 for i in $(cat debian7_package.list)
 do
   echo "apt-get install $i" 
