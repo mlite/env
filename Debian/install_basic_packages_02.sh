d7652dcec163b74eb9a1d6e0f2ca2b9953e0aaf4
mlite <mlite@users.noreply.github.com>
2015-01-01 23:15:44 -0800

    automate more steps

--- Debian/install_basic_packages_02.sh
+++ Debian/install_basic_packages_02.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+echo "export PATH=.:\$HOME/local/bin:\$HOME/.cabal/bin:\$HOME/llvm-install/bin:\$PATH" >> $HOME/.bashrc
+source $HOME/.bashrc
 echo "sudo: type the password of $USER"
 sudo apt-get -y install build-essential
-sudo apt-get -y install git
