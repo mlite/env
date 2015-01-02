c2253aff57b869b5e392ce876b0624a5e01499c2
mlite <mlite@users.noreply.github.com>
2015-01-01 21:30:07 -0800

    group them in one installation script:install_all.sh

--- Haskell/debian7_install_01.sh
+++ Haskell/debian7_install_01.sh
@@ -1,7 +1,9 @@
 #!/bin/bash 
 
+echo "sudo $USER, please type the password of $USER"
+sudo
 for i in $(cat debian7_package.list)
 do
   echo "apt-get install $i" 
-  apt-get install $i
+  sudo apt-get install $i
 done
