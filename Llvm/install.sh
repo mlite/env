e9f444522ef4982712ef6311d7956e50701b18fc
Ning Wang <email@ningwang.org>
2015-01-01 23:57:54 -0800

    remove unnessary packages

--- Llvm/install.sh
+++ Llvm/install.sh
@@ -3,17 +3,10 @@ if [ ! -f llvm-3.5.0.src.tar.xz ]; then
   wget http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz
 fi
 
-if [ ! -f test-suite-3.5.0.src.tar.xz ]; then
-  wget http://llvm.org/releases/3.5.0/test-suite-3.5.0.src.tar.xz
-fi
 
 rm -rf llvm-3.5.0.src
 tar xvfJ llvm-3.5.0.src.tar.xz
-cd llvm-3.5.0.src/projects 
-tar xvfJ ../../test-suite-3.5.0.src.tar.xz
 
-
-cd ../../
 
 rm -rf llvm-build
 mkdir llvm-build
@@ -22,3 +15,4 @@ mkdir llvm-build
 cd llvm-build
 ../llvm-3.5.0.src/configure --prefix=/home/$USER/llvm-install
 make
+make -C test
