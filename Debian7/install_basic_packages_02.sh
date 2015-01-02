0d5989689502ec34d964ee3a04e6a2aa668e3cde
mlite <mlite@users.noreply.github.com>
2015-01-01 22:54:19 -0800

    refactor the code split the script of installing emacs and leskah into a different folder

--- Debian7/install_basic_packages_02.sh
+++ Debian7/install_basic_packages_02.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo "sudo: type the password of $USER"
 sudo apt-get -y install build-essential
+sudo apt-get -y install git
---
+++ HaskellIDE/debian7_install_01.sh
@@ -0,0 +1,10 @@
+#!/bin/bash 
+
+echo "sudo $USER, please type the password of $USER"
+sudo
+for i in $(cat debian7_package.list)
+do
+  cmd="apt-get -y install $i"
+  echo "$cmd"
+  sudo $cmd
+done
