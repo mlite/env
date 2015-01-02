122ef0bb233778dd25d7dc3f5abc0ca3b01b8e58
Ning Wang <email@ningwang.org>
2015-01-01 23:37:29 -0800

    fixes the file name

--- Haskell/debian_install_01.sh
+++ Haskell/debian_install_01.sh
@@ -1,7 +1,7 @@
 #!/bin/bash 
 
 echo "sudo $USER, please type the password of $USER"
-for i in $(cat debian7_package.list)
+for i in $(cat debian_package.list)
 do
   cmd="apt-get -y install $i"
   echo "$cmd"
