020dc2dbd03393a6e9d2bc49b36dab7000f17998
mlite <mlite@users.noreply.github.com>
2015-01-01 22:05:08 -0800

    add configure and make steps

--- Llvm/install.sh
+++ Llvm/install.sh
@@ -11,3 +11,14 @@ rm -rf llvm-3.5.0.src
 tar xvfJ llvm-3.5.0.src.tar.xz
 cd llvm-3.5.0.src/projects 
 tar xvfJ ../../test-suite-3.5.0.src.tar.xz
+
+
+cd ../../
+
+rm -rf llvm-build
+mkdir llvm-build
+
+
+cd llvm-build
+../llvm-3.5.0.src/configure --prefix=/home/$USER/llvm-install
+make
