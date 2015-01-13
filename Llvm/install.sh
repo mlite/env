cd115188a34f7bbddf4015ac8db5a50e29a6d930
mlite <mlite@users.noreply.github.com>
2015-01-12 23:09:08 -0800

    install Clang and test-suite

--- Llvm/install.sh
+++ Llvm/install.sh
@@ -3,10 +3,30 @@ if [ ! -f llvm-3.5.0.src.tar.xz ]; then
   wget http://llvm.org/releases/3.5.0/llvm-3.5.0.src.tar.xz
 fi
 
+if [ ! -f cfe-3.5.0.src.tar.xz ]; then
+  wget http://llvm.org/releases/3.5.0/cfe-3.5.0.src.tar.xz
+fi
 
+if [ ! -f compiler-rt-3.5.0.src.tar.xz ]; then
+  wget http://llvm.org/releases/3.5.0/compiler-rt-3.5.0.src.tar.xz
+fi
+
+if [ ! -f test-suite-3.5.0.src.tar.xz ]; then
+  wget http://llvm.org/releases/3.5.0/test-suite-3.5.0.src.tar.xz
+fi
+
+
+
+
+echo "unzip llvm ..."
 rm -rf llvm-3.5.0.src
 tar xvfJ llvm-3.5.0.src.tar.xz
 
+echo "unzip clang ..."
+tar xvfJ cfe-3.5.0.src.tar.xz -C llvm-3.5.0.src/tools
+
+echo "unzip test-suite ..."
+tar xvfJ ../../test-suite-3.5.0.src.tar.xz -C llvm-3.5.0.src/projects
 
 rm -rf llvm-build
 mkdir llvm-build
@@ -15,4 +35,6 @@ mkdir llvm-build
 cd llvm-build
 ../llvm-3.5.0.src/configure --prefix=/home/$USER/llvm-install
 make
-make -C test
+make check-all
+
+
