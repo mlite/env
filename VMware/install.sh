702b3400e7447f35147872b3acb1ee70560f4afe
Ning Wang <email@ningwang.org>
2015-06-04 03:13:47 -0700

    add linux header installation

--- VMware/install.sh
+++ VMware/install.sh
@@ -1,2 +1,3 @@
+sudo apt-get install linux-headers-$(uname -r)
 wget http://download3.vmware.com/software/player/file/VMware-Player-7.0.0-2305329.x86_64.bundle
 chmod +x VMware-Player-7.0.0-2305329.x86_64.bundle
