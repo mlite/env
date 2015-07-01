da528ccbb87b1aacf1662d733b2aedfb9f9f6b08
mlite <mlite@users.noreply.github.com>
2015-07-01 01:21:20 -0700

    add scripts to build  a docker images

---
+++ docker/ghc_cabal_install.sh
@@ -0,0 +1,30 @@
+#!/bin/bash -x
+
+cd $HOME
+mkdir $HOME/local
+
+NUMJOBS=`nproc`
+
+# download ghc
+if [ ! -f ghc-7.8.3-src.tar.bz2 ]; then
+    wget https://www.haskell.org/ghc/dist/7.8.3/ghc-7.8.3-src.tar.bz2
+fi
+
+rm -rf ghc-7.8.3
+bzip2 -dc ghc-7.8.3-src.tar.bz2 | tar xvf -
+
+export PATH=$HOME/.cabal/bin:$PATH
+ 
+cd ghc-7.8.3
+./configure --prefix=$HOME/local
+make -j ${NUMJOBS}
+make install
+
+
+# we need to use ghc-7.8.3 to install cabal-install
+export PATH=$HOME/local/bin:$PATH
+
+cabal update
+cabal install cabal-install
+
+echo "export PATH=$HOME/local/bin:$HOME/.cabal/bin:$PATH" >> $HOME/.bashrc
