ff16104bca7ee2ffc5b4421d4024cdc04b5da130
mlite <mlite@users.noreply.github.com>
2015-07-03 10:40:02 -0700

    need to pull out env

--- docker/build_debian.sh
+++ docker/build_debian.sh
@@ -49,6 +49,7 @@ chroot $chroot_dir apt-get clean
 chroot $chroot_dir apt-get autoremove
 
 ### install build enviroment
+chroot $chroot_dir git clone https://github.com/mlite/env
 cp install_env.sh $chroot_dir/root
 chroot $chroot_dir sh /root/install_env.sh
 
