61dfa1c7c85e499c407ef7a2504555446062bd18
Ning Wang <email@ningwang.org>
2015-07-03 16:12:52 -0700

    fixes script

--- Haskell/install.sh
+++ Haskell/install.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-debian_install_01.sh
-ghc_cabal_install_02.sh
+SCRIPT_PATH=$(dirname $(readlink -f $0))
+sh ${SCRIPT_PATH}/debian_install_01.sh
+sh ${SCRIPT_PATH}/ghc_cabal_install_02.sh
 source $HOME/.bashrc
