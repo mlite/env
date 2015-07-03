73e21dc8312779816ab9d73927caeccf97f19ffb
mlite <mlite@users.noreply.github.com>
2015-07-03 08:13:47 -0700

    bootstrap cabal from source

--- docker/ghc_cabal_install.sh
+++ docker/ghc_cabal_install.sh
@@ -13,18 +13,19 @@ fi
 rm -rf ghc-7.8.3
 bzip2 -dc ghc-7.8.3-src.tar.bz2 | tar xvf -
 
-export PATH=$HOME/.cabal/bin:$PATH
- 
 cd ghc-7.8.3
 ./configure --prefix=$HOME/local/ghc
 make -j ${NUMJOBS}
 make install
+cd ..
 
 
 # we need to use ghc-7.8.3 to install cabal-install
 export PATH=$HOME/local/ghc/bin:$PATH
 
-cabal update
-cabal install cabal-install
+wget https://www.haskell.org/cabal/release/cabal-install-1.22.6.0/cabal-install-1.22.6.0.tar.gz
+gzip -dc cabal-install-1.22.6.0.tar.gz | tar xvf -
+cd cabal-install-1.22.6.0
+sh ./bootstrap.sh
 
 echo "export PATH=$HOME/local/ghc/bin:$HOME/.cabal/bin:$PATH" >> $HOME/.bashrc
