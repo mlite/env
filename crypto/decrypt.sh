f166b12d53ef6299dd5933dbe62688dbab8eb49d
Ning Wang <email@ningwang.org>
2016-02-16 23:11:11 -0800

    fixes the script

--- crypto/decrypt.sh
+++ crypto/decrypt.sh
@@ -1,13 +1,13 @@
-#!/bin/bash
-if [ "$#" -ne 3 ]; then
-    echo "usage: $0 <the gpg file> <the password> <the destination folder>"
+#!/bin/bash -x
+if [ "$#" -ne 2 ]; then
+    echo "usage: $0 <the gpg file> <the destination folder>"
     exit 1;
 fi
 
 bname=`basename $1 .gpg`
 
-gpg --cipher-algo AES256 --symmetric --passphrase $2 -d $1 --output ${bname}
+gpg --output ${bname} --decrypt $1 
 
-tar xvfJ ${bname} -C $3
+tar xvfJ ${bname} --directory=$2
 
 
