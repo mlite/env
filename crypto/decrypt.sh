5bee57131c5608eb8b50f1a67c92434149b71f91
mlite <mlite@users.noreply.github.com>
2016-02-21 20:34:46 -0800

    add passphrase

--- crypto/decrypt.sh
+++ crypto/decrypt.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -x
-if [ "$#" -ne 2 ]; then
-    echo "usage: $0 <the gpg file> <the destination folder>"
+if [ "$#" -ne 3 ]; then
+    echo "usage: $0 <the gpg file> <the destination folder> <passphrase>"
     exit 1;
 fi
 
 bname=`basename $1 .gpg`
 
-gpg --output ${bname} --decrypt $1 
+gpg --output ${bname} --passphrase $2 --decrypt $1 
 
 tar xvfJ ${bname} --directory=$2
 
