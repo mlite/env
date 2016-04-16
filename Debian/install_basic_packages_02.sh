b7ad0dc24c24e06291a57b0d49f1d6bae7f8d3cb
mlite <mlite@users.noreply.github.com>
2016-04-16 13:04:56 -0700

    python-dev is needed for buildbot

--- Debian/install_basic_packages_02.sh
+++ Debian/install_basic_packages_02.sh
@@ -3,5 +3,6 @@ echo "export PATH=.:\$PATH" >> $HOME/.bashrc
 source $HOME/.bashrc
 echo "sudo: type the password of $USER"
 sudo apt-get -y install build-essential
+sudo apt-get -y install python-dev
 sudo apt-get -y install unzip
 sudo apt-get -y install cmake
