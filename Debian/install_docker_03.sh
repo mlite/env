e0edb1a6c9d06324bb2d88b6bdd34296ba5f4ecc
mlite <mlite@users.noreply.github.com>
2017-10-05 14:01:59 -0700

    add the script to install docker ce for debian 8

---
+++ Debian/install_docker_03.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+sudo apt-get update
+
+#add repository for docker-ce
+sudo apt-get install \
+     apt-transport-https \
+     ca-certificates \
+     curl \
+     gnupg2 \
+     software-properties-common -y
+
+curl -fsSL https://download.docker.com/linux/$(. /etc/os-release; echo "$ID")/gpg | sudo apt-key add -
+
+sudo apt-key fingerprint 0EBFCD88
+
+sudo add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/$(. /etc/os-release; echo "$ID") \
+   $(lsb_release -cs) \
+   stable"
+
+
+#install docker-ce
+sudo apt-get update
+sudo apt-get install docker-ce -y
+
+#verify it works
+sudo docker run hello-world
+
+
+# install docker compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
