a21698a84f0dfd17ef75cbac453562b7ea5457a6
Ning Wang <email@ningwang.org>
2017-10-05 19:08:29 -0700

    install from backports

--- Debian/install_java8_04.sh
+++ Debian/install_java8_04.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 sudo echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list
 sudo apt-get update
-sudo apt-get install -y openjdk-8-jdk
+sudo apt-get install -t jessie-backports -y openjdk-8-jre-headless ca-certificates-java
 sudo update-alternative --config java
