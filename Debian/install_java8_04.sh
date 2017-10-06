0fd2581bf9a6fda979bc4a87e1d1f18c2ce56151
mlite <mlite@users.noreply.github.com>
2017-10-05 19:14:15 -0700

    delete non-exist executables

--- Debian/install_java8_04.sh
+++ Debian/install_java8_04.sh
@@ -2,4 +2,3 @@
 sudo echo "deb http://http.debian.net/debian jessie-backports main" >> /etc/apt/sources.list
 sudo apt-get update
 sudo apt-get install -t jessie-backports -y openjdk-8-jre-headless ca-certificates-java
-sudo update-alternative --config java
