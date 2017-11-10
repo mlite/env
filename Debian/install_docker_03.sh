6ec922b4fcb541ff20b14dd37c2276f56fffa7e9
Ning Wang <email@ningwang.org>
2017-11-09 20:04:46 -0800

    seperate the installation of docker-compose

--- Debian/install_docker_03.sh
+++ Debian/install_docker_03.sh
@@ -28,6 +28,6 @@ sudo docker run hello-world
 
 
 # install docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.16.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
