2f82ee059bbb685f0e118f71d0eae9c43d0e2fb6
Ning Wang <email@ningwang.org>
2017-11-20 10:40:40 -0800

    add the current user to docker group

--- Debian/install_docker_03.sh
+++ Debian/install_docker_03.sh
@@ -31,3 +31,6 @@ sudo docker run hello-world
 sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 docker-compose --version
+
+
+sudo -aG docker $USER
