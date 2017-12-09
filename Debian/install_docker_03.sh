4899e11074150914f40f731cdbe8587fd9fdd98e
Ning Wang <email@ningwang.org>
2017-12-09 11:34:15 -0800

    docker-compose is installed in a different script

--- Debian/install_docker_03.sh
+++ Debian/install_docker_03.sh
@@ -27,10 +27,6 @@ sudo apt-get install docker-ce -y
 sudo docker run hello-world
 
 
-# install docker compose
-sudo curl -L https://github.com/docker/compose/releases/download/1.17.1/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
-sudo chmod +x /usr/local/bin/docker-compose
-docker-compose --version
 
 
-sudo -aG docker $USER
+sudo useradd -G docker $USER
