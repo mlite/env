750b4e0f00dd0729da595aa824831043937ccd18
mlite <mlite@users.noreply.github.com>
2017-12-10 11:51:17 -0800

    comment on the possiblity of starting worker

--- Buildbot/worker/install.sh
+++ Buildbot/worker/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-mkdir -p ~/tmp/bb-worker
-cd ~/tmp/bb-worker
+mkdir -p ~/bb-worker
+cd ~/bb-worker
 
 virtualenv --no-site-packages sandbox
 source sandbox/bin/activate
@@ -12,4 +12,6 @@ pip install setuptools-trial
 
 buildbot-worker create-worker worker localhost example-worker pass
 
+# This might fail depending on the master configration
+# If it fails, please fix the worker with the correct parameters
 buildbot-worker start worker
