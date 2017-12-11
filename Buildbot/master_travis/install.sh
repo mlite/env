4b6b18c1e35376a805dac2f917be2a5dcba4820e
mlite <mlite@users.noreply.github.com>
2017-12-11 09:01:42 -0800

    add the missing steps

--- Buildbot/master_travis/install.sh
+++ Buildbot/master_travis/install.sh
@@ -5,7 +5,8 @@ virtualenv --no-site-packages sandbox
 source sandbox/bin/activate
 
 pip install --upgrade pip
+pip install service_identity
 pip install buildbot_travis
 buildbot create-master master
-#cp master/master.cfg.sample master/master.cfg
+cp master/master.cfg.sample master/master.cfg
 buildbot start master
