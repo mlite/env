a30d37958357dd44fc3056ebcc0cb15f06badee8
Ning Wang <email@ningwang.org>
2017-12-21 11:04:00 -0800

    fixes systemd files

--- Buildbot/worker/create_user.sh
+++ Buildbot/worker/create_user.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 sudo addgroup --system buildbot
 sudo adduser buildbot_worker --system --ingroup buildbot --shell /bin/bash
-sudo --login --user buildbot_worker
+#sudo --login --user buildbot_worker
