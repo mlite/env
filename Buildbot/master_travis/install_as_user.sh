ed9c568c8009149f846f85228f768e8330671a86
mlite <mlite@users.noreply.github.com>
2017-12-21 16:33:45 -0800

    pin the version

--- Buildbot/master_travis/install_as_user.sh
+++ Buildbot/master_travis/install_as_user.sh
@@ -5,7 +5,7 @@ source sandbox/bin/activate
 
 pip install --upgrade pip
 pip install service_identity
-pip install 'buildbot[bundle]'
+pip install 'buildbot[bundle]==0.9.14'
 pip install buildbot_travis
 bbtravis create-master master
 buildbot start master
