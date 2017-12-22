35ef4a3f310b18fb57757bbff06fc63b68fb93de
mlite <mlite@users.noreply.github.com>
2017-12-21 16:12:48 -0800

    create a special user for buildbot master

---
+++ Buildbot/master_travis/install_as_user.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+mkdir -p ~/master
+cd ~/master
+virtualenv --no-site-packages sandbox
+source sandbox/bin/activate
+
+pip install --upgrade pip
+pip install service_identity
+pip install 'buildbot[bundle]'
+pip install buildbot_travis
+bbtravis create-master master
+buildbot start master
