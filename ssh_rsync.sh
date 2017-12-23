6c3b39addf0b92ff852c3d7f93454cf27adc2247
Ning Wang <email@ningwang.org>
2017-12-23 10:22:06 -0800

    add error checkings

--- ssh_rsync.sh
+++ ssh_rsync.sh
@@ -1,7 +1,12 @@
-#!/bin/bash
+#!/bin/bash -x
 mypath=$(dirname $(readlink -f $0))
 
-while [ $# ]; do
+help() {
+    echo "Usage: $0 --remote-host <ip> --remote-user <usr> --remote-dir <path> --local-dir <path>"
+    exit 1
+}
+
+while [ $# -ne 0 ]; do
     case $1 in
 	"--remote-dir")
 	    shift
@@ -20,10 +25,16 @@ while [ $# ]; do
 	    LOCAL_DIR=$1
 	    ;;
 	*)
-	    echo "Usage: $0 --remote-host <ip> --remote-user <usr> --remote-dir <path> --local-dir <path>"
+            help
+            ;;
     esac
     shift
 done
 
+if [ -z $REMOTE_USR ] || [ -z $REMOTE_HOST ] || [ -z $LOCAL_DIR ]; then
+  help  
+fi
+
+${REMOTE_DIR:-"~/"}
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" \
       --progress $LOCAL_DIR ${REMOTE_USR}@${REMOTE_HOST}:${REMOTE_DIR}
