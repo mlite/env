b7fd90f9fe81d06e115de70cf9f705f48c24f816
mlite <mlite@users.noreply.github.com>
2019-07-02 19:32:44 -0700

    need m4 to preprocessing configuration files

--- Debian/install_basic_packages_02.sh
+++ Debian/install_basic_packages_02.sh
@@ -5,4 +5,4 @@ echo "sudo: type the password of $USER"
 sudo apt-get -y install build-essential
 sudo apt-get -y install python-dev
 sudo apt-get -y install unzip
-sudo apt-get -y install cmake
+sudo apt-get -y install cmake m4
