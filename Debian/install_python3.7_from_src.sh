7e11bb71a9dc95ae963e85de471232768f335ede
mlite <mlite@users.noreply.github.com>
2020-04-28 20:35:23 -0700

    needs to build the shared libs

--- Debian/install_python3.7_from_src.sh
+++ Debian/install_python3.7_from_src.sh
@@ -9,10 +9,11 @@ cd /usr/src
 wget https://www.openssl.org/source/openssl-1.1.1a.tar.gz
 tar -xvf openssl-1.1.1a.tar.gz
 cd openssl-1.1.1a
-./config --prefix=/usr/local/openssl --openssldir=/usr/local/openssl
+./config --shared --prefix=/usr/local/openssl --openssldir=/usr/local/openssl
 make
 make install
 
+export LD_LIBRARY_PATH=/usr/local/openssl/lib
 
 cd /usr/src
 sudo wget https://www.python.org/ftp/python/3.7.4/Python-3.7.4.tgz
